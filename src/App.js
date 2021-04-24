0000d1e3f6081832fee891a95c86d11dcf528fc3
tanakorn0314 <tanakorn0412@gmail.com>
2021-04-24 15:58:40 +0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import firebase from 'firebase';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [search, setSearch] = useState('');
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    const db = firebase.firestore().collection('chineselanguage');
+    db.onSnapshot((snapshot) => {
+      const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setWords(data.sort((a, b) => Number(a.id) - Number(b.id)));
+      console.log('data', data)
+    })
+  }, []);
+
+  const filteredData = words.filter((word) => {
+    const meaning = word['ความหมาย']
+    const vocab = word['คำศัพท์']
+    const sentence = word['ตัวอย่างประโยค']
+    const pinin = word['พินอิน']
+    return meaning.indexOf(search) >= 0 || vocab.indexOf(search) >= 0 || sentence.indexOf(search) >= 0 || pinin.indexOf(search) >= 0
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-10 flex justify-center bg-blue-50 h-screen">
+
+      <div className="max-w-2xl w-full bg-white p-4 border rounded shadow-sm">
+
+        <h1 class="text-center mb-4 font-bold text-2xl">Chinese Language</h1>
+
+        <div class="mb-4 border rounded px-2 py-1 flex space-x-1">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+          </svg>
+          <input className="focus:outline-none" placeholder="ค้นหาคำศัพท์" onChange={e => setSearch(e.target.value)} />
+        </div>
+
+        <table>
+          <thead>
+            <tr>
+              <th className="border p-2" width="10%">#</th>
+              <th className="border p-2" width="30%">ความหมาย</th>
+              <th className="border p-2" width="20%">คำศัพท์</th>
+              <th className="border p-2" width="30%">ตัวอย่างประโยค</th>
+              <th className="border p-2" width="10%">พินอิน</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData.map((word, index) => (
+              <tr key={index}>
+                <td className="border p-2" width="10%">{index + 1}</td>
+                <td className="border p-2" width="30%">{word['ความหมาย']}</td>
+                <td className="border p-2" width="20%">{word['คำศัพท์']}</td>
+                <td className="border p-2" width="30%">{word['ตัวอย่างประโยค']}</td>
+                <td className="border p-2" width="10%">{word['พินอิน']}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+      </div>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AaK39Y",
+  authDomain: "chinp.com",
+  databaseURL: "httpsseio.com",
+  projectId: "chi4b908",
+  storageBucket: "chineppspot.com",
+  messagingSenderId: "129647",
+  appId: "1:fafa",
+  measurementId: "G-Q8"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +25,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
